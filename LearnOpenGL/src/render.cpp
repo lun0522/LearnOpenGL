dbf9281c4a5cc1731d3073370428cae84ffc946d
lun0522 <pl557@cornell.edu>
2018-03-21 18:04:46 -0400

    Lighting

--- LearnOpenGL/src/render.cpp
+++ LearnOpenGL/src/render.cpp
@@ -18,6 +18,8 @@ using std::string;
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+const int NUM_CUBE = 5;
+const int NUM_POINT_LIGHTS = 3;
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastFrame = 0.0f;
@@ -219,16 +221,6 @@ void Render::renderLoop() {
     glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind VBO
     
     // ------------------------------------
-    // coordinates
-    
-    // object to world
-    glm::mat4 light = glm::mat4(1.0f);
-    glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 1.5f);
-    light = glm::translate(light, lightPos);
-    light = glm::scale(light, glm::vec3(0.2f));
-    glm::mat4 rotator = glm::mat4(1.0f);
-    
-    // ------------------------------------
     // texture
     
     // OpenGL expects 0.0 of y axis lays on the top, so flip the image
@@ -245,13 +237,29 @@ void Render::renderLoop() {
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, specularMap);
     
-    glm::vec3 lightColor = glm::vec3(1.0f);
+    glm::vec3 lightColor = glm::vec3(0.3f);
     glm::vec3 ambientColor = lightColor * 0.2f;
     glm::vec3 diffuseColor = lightColor * 0.8f;
     
-    lightShader.use();
-    lightShader.setVec3("lightColor", glm::value_ptr(lightColor));
-    lightShader.setMat4("model", glm::value_ptr(light));
+    glm::vec3 cubePos[NUM_CUBE] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f,  1.8f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f)
+    };
+    
+    glm::vec3 pointLightPos[NUM_POINT_LIGHTS] = {
+        glm::vec3( 0.7f,  0.2f,  2.0f),
+        glm::vec3( 2.3f, -1.5f, -4.0f),
+        glm::vec3(-4.0f,  2.0f, -7.0f)
+    };
+    
+    glm::vec3 pointLightColor[NUM_POINT_LIGHTS] = {
+        glm::vec3(1.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f),
+        glm::vec3(0.0f, 0.0f, 1.0f)
+    };
     
     rotatorShader.use();
     rotatorShader.setInt("material.emission", 0); // tell OpenGL which sampler corresponds to which texture
@@ -259,9 +267,38 @@ void Render::renderLoop() {
     rotatorShader.setInt("material.specular", 1);
     rotatorShader.setFloat("material.shininess", 0.4f * 128.0f);
     rotatorShader.setVec3("material.specular", 0.508273f, 0.508273f, 0.508273f);
-    rotatorShader.setVec3("light.ambient", glm::value_ptr(ambientColor));
-    rotatorShader.setVec3("light.diffuse", glm::value_ptr(diffuseColor));
-    rotatorShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+    
+    // directional light
+    rotatorShader.setVec3("dirLight.direction", -1.0f, -1.0f, -1.0f);
+    rotatorShader.setVec3("dirLight.ambient", 0.0f, 0.0f, 0.0f);
+    rotatorShader.setVec3("dirLight.diffuse", glm::value_ptr(ambientColor));
+    rotatorShader.setVec3("dirLight.specular", glm::value_ptr(lightColor));
+    
+    // point lights
+    for (int i = 0; i < NUM_POINT_LIGHTS; ++i) {
+        string light = "pointLights[" + std::to_string(i) + "]";
+        rotatorShader.setFloat(light + ".constant", 1.0f);
+        rotatorShader.setFloat(light + ".linear", 0.09f);
+        rotatorShader.setFloat(light + ".quadratic", 0.032f);
+        glm::vec3 ambient = ambientColor + pointLightColor[i];
+        glm::vec3 diffuse = diffuseColor + pointLightColor[i];
+        glm::vec3 specular = lightColor + pointLightColor[i];
+        rotatorShader.setVec3(light + ".ambient", glm::value_ptr(ambient));
+        rotatorShader.setVec3(light + ".diffuse", glm::value_ptr(diffuse));
+        rotatorShader.setVec3(light + ".specular", glm::value_ptr(specular));
+    }
+    
+    // spot light
+    rotatorShader.setVec3("spotLight.position", 0.0f, 0.0f, 0.0f);
+    rotatorShader.setVec3("spotLight.direction", 0.0f, 0.0f, -1.0f);
+    rotatorShader.setFloat("spotLight.innerCutOff", glm::cos(glm::radians(12.5f)));
+    rotatorShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(17.5f)));
+    rotatorShader.setFloat("spotLight.constant", 1.0f);
+    rotatorShader.setFloat("spotLight.linear", 0.09f);
+    rotatorShader.setFloat("spotLight.quadratic", 0.032f);
+    rotatorShader.setVec3("spotLight.ambient", glm::value_ptr(ambientColor));
+    rotatorShader.setVec3("spotLight.diffuse", glm::value_ptr(diffuseColor));
+    rotatorShader.setVec3("spotLight.specular", glm::value_ptr(lightColor));
     
     while (!glfwWindowShouldClose(window)) { // until user hit close
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -269,26 +306,40 @@ void Render::renderLoop() {
         
         processKeyboardInput();
         
+        glBindVertexArray(rotatorVAO);
         glm::mat4 view = camera.getViewMatrix();
         glm::mat4 projection = camera.getProjectionMatrix();
         
         lightShader.use();
         lightShader.setMat4("view", glm::value_ptr(view));
         lightShader.setMat4("projection", glm::value_ptr(projection));
-        glBindVertexArray(rotatorVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        
+        // only point lights
+        for (int i = 0; i < NUM_POINT_LIGHTS; ++i) {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), pointLightPos[i]);
+            model = glm::scale(model, glm::vec3(0.2f));
+            lightShader.setMat4("model", glm::value_ptr(model));
+            lightShader.setVec3("lightColor", glm::value_ptr(pointLightColor[i]));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         
         rotatorShader.use();
-        rotator = glm::rotate(rotator, glm::radians(1.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        glm::vec3 lightInView = glm::vec3(view * glm::vec4(lightPos, 1.0f));
-        glm::mat3 normal = glm::transpose(glm::inverse(glm::mat3(view * rotator)));
-        rotatorShader.setFloat("time", lastFrame);
-        rotatorShader.setVec3("light.position", glm::value_ptr(lightInView));
-        rotatorShader.setMat4("model", glm::value_ptr(rotator));
-        rotatorShader.setMat3("normal", glm::value_ptr(normal));
+        for (int i = 0; i < NUM_POINT_LIGHTS; ++i) {
+            glm::vec3 lightInView = glm::vec3(view * glm::vec4(pointLightPos[i], 1.0f));
+            rotatorShader.setVec3("pointLights[" + std::to_string(i) + "].position", glm::value_ptr(lightInView));
+        }
+        rotatorShader.setFloat("emitIntensity", glm::sin(lastFrame * 3) + 0.5);
         rotatorShader.setMat4("view", glm::value_ptr(view));
         rotatorShader.setMat4("projection", glm::value_ptr(projection));
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        
+        for (int i = 0; i < NUM_CUBE; ++i) {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), cubePos[i]);
+            model = glm::rotate(model, glm::radians(lastFrame * 20.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
+            glm::mat3 normal = glm::transpose(glm::inverse(glm::mat3(view * model)));
+            rotatorShader.setMat4("model", glm::value_ptr(model));
+            rotatorShader.setMat3("normal", glm::value_ptr(normal));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         
         glfwSwapBuffers(window); // use color buffer to draw
         glfwPollEvents(); // check events (keyboard, mouse, ...)
--- LearnOpenGL/src/shaders/shader_rotator.fs
+++ LearnOpenGL/src/shaders/shader_rotator.fs
@@ -1,16 +1,46 @@
 #version 330 core
 
+#define NUM_POINT_LIGHTS 3
+
 in vec3 norm;
 in vec3 fragPos;
 in vec2 texCoord;
 
 out vec4 fragColor;
 
-struct Light {
-    vec3 position;
+struct DirLight {
+    vec3 direction;
+    
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+};
+
+struct PointLight {
+    vec3 position;
+    
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+struct SpotLight {
+    vec3 position;
+    vec3 direction;
+    float innerCutOff;
+    float outerCutOff;
+    
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 struct Material {
@@ -20,28 +50,74 @@ struct Material {
     float shininess;
 };
 
-uniform Light light;
+uniform DirLight dirLight;
+uniform PointLight pointLights[NUM_POINT_LIGHTS];
+uniform SpotLight spotLight;
 uniform Material material;
-uniform float time;
+uniform float emitIntensity;
+
+vec3 calcDirLight(DirLight light, vec3 normal, vec3 viewDir) {
+    vec3 lightDir = normalize(-light.direction);
+    float diff = max(dot(normal, lightDir), 0.0); // for diffuse
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    
+    vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, texCoord));
+    vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, texCoord));
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, texCoord));
+    
+    return ambient + diffuse + specular;
+}
+
+vec3 calcPointLight(PointLight light, vec3 normal, vec3 viewDir) {
+    vec3 lightDir = normalize(light.position - fragPos);
+    float diff = max(dot(normal, lightDir), 0.0); // for diffuse
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess); // for specular
+    
+    // attenuation
+    float lightDist = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * lightDist + light.quadratic * (lightDist * lightDist));
+    
+    vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, texCoord));
+    vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, texCoord));
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, texCoord));
+    
+    return (ambient + diffuse + specular) * attenuation;
+}
+
+vec3 calcSpotLight(SpotLight light, vec3 normal, vec3 viewDir) {
+    vec3 lightDir = normalize(light.position - fragPos);
+    float diff = max(dot(normal, lightDir), 0.0); // for diffuse
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess); // for specular
+    
+    // attenuation
+    float lightDist = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * lightDist + light.quadratic * (lightDist * lightDist));
+    
+    // intensity
+    float theta = dot(-lightDir, normalize(light.direction));
+    float epsilon = light.innerCutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    
+    vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, texCoord));
+    vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, texCoord));
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, texCoord));
+    
+    return (ambient + (diffuse + specular) * intensity) * attenuation;
+}
 
 void main() {
-    // emission
-    vec3 emission = vec3(texture(material.emission, texCoord)) * (sin(time * 3) + 0.5);
-    
-    // ambient
-    vec3 ambient = vec3(texture(material.diffuse, texCoord)) * light.ambient;
-    
-    // diffuse
-    vec3 normalDir = normalize(norm);
-    vec3 lightDir = normalize(light.position - fragPos);
-    float diff = max(dot(normalDir, lightDir), 0.0);
-    vec3 diffuse = diff * vec3(texture(material.diffuse, texCoord)) * light.diffuse;
-    
-    // specular
+    vec3 normal = normalize(norm);
     vec3 viewDir = normalize(-fragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = spec * vec3(texture(material.specular, texCoord)) * light.specular;
     
-    fragColor = vec4(emission + ambient + diffuse + specular, 1.0);
+    vec3 outColor = vec3(0.0);
+    outColor += calcDirLight(dirLight, normal, viewDir);
+    for (int i = 0; i < NUM_POINT_LIGHTS; ++i)
+        outColor += calcPointLight(pointLights[i], normal, viewDir);
+    outColor += calcSpotLight(spotLight, normal, viewDir);
+    outColor += emitIntensity  * vec3(texture(material.emission, texCoord)); // emission
+    
+    fragColor = vec4(outColor, 1.0);
 }
